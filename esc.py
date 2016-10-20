243edb156dd66d3a83254385aa364d8d2e578bc9
minhyein <player_02@naver.com>
2016-10-20 13:50:11 +0900

    code

--- esc.py
+++ esc.py
@@ -4,7 +4,7 @@ import random
 class Char:
     image = None
 
-    RUN, JUMP = 0, 1
+    Attack1, Attack2, RUN, JUMP = 0, 1, 2, 3
 
 
     def handle_run(self):
@@ -15,49 +15,67 @@ class Char:
             self.x = 0
 
     def handle_jump(self):
+        self.frame = 5
         self.ychange += 1
         if self.ychange < 10:
             self.y += 5
             self.state = self.JUMP
         elif self.ychange >= 10 and self.ychange < 20:
             self.y -= 5
+            if self.y < 200:
+                self.y = 200
             self.state = self.JUMP
         elif self.ychange == 20:
             self.ychange = 0
             self.state = self.RUN
+    def handle_attack1(self):
+        self.attackframe += 1
+        if self.attackframe == 6:
+            self.state = self.RUN
+            self.attackframe = 0
+
+    def handle_attack2(self):
+        self.attackframe += 1
+        if self.attackframe == 6:
+            self.state = self.RUN
+            self.attackframe = 0
 
     handle_state = {
         RUN: handle_run,
-        JUMP: handle_jump
+        JUMP: handle_jump,
+        Attack1: handle_attack1,
+        Attack2: handle_attack2
     }
 
     def update(self):
-        self.frame = (self.frame + 1) % 8
+        self.frame = (self.frame + 1) % 6
         self.handle_state[self.state](self)
 
     def __init__(self):
-        self.x, self.y = 100, 90
+        self.x, self.y = 100, 200
         self.frame = 0
         self.xchange, self.ychange = 0, 0
+        self.attackcount = 0
+        self.attackframe = 0
         self.state = self.RUN
         if Char.image == None:
-            Char.image = load_image('run_animation.png')
+            Char.image = load_image('Untitled-2.png')
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 150, self.state * 150, 150, 150, self.x, self.y)
 
-class Grass:
+class Background:
     def __init__(self):
-        self.image = load_image('grass.png')
+        self.image = load_image('background.png')
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(400, 300)
 
 class Zombie:
     image = None
 
     def __init__(self):
-        self.x, self.y = random.randint(500, 1400), 90
+        self.x, self.y = random.randint(500, 1400), 200
         self.frame = random.randint(0, 6)
         if Zombie.image == None:
             Zombie.image = load_image('Monster-zombie.png')
@@ -67,13 +85,13 @@ class Zombie:
         self.x -= 2
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 120, 0, 120, 120, self.x, self.y)
 
 class Lizard:
     image = None
 
     def __init__(self):
-        self.x, self.y = random.randint(500, 1400), 90
+        self.x, self.y = random.randint(500, 1400), 200
         self.frame = random.randint(0, 5)
         if Lizard.image == None:
             Lizard.image = load_image('Monster-lizard.png')
@@ -83,8 +101,45 @@ class Lizard:
         self.x -= 2
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 200, 0, 200, 140, self.x, self.y)
 
+class Octopus:
+    image = None
+    Attack, Run = 0, 1
+
+    def __init__(self):
+        self.x, self.y = random.randint(200, 1400), 200
+        self.frame = random.randint(0, 5)
+        self.runframe = 0
+        self.attackframe = 0
+        self.state = self.Run
+        if Octopus.image == None:
+            Octopus.image = load_image('octopus.png')
+
+    def handle_run(self):
+        self.x -= 2
+        self.runframe += 1
+        if self.runframe == 50:
+            self.state = self.Attack
+            self.attackframe = 0
+
+    def handle_attack(self):
+        self.attackframe += 1
+        if self.attackframe == 12:
+            self.state = self.Run
+            self.runframe = 0
+
+    handle_state = {
+        Run: handle_run,
+        Attack: handle_attack
+    }
+
+    def update(self):
+        self.frame = (self.frame + 1) % 6
+        self.handle_state[self.state](self)
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 150, self.state  * 150, 150, 150, self.x, self.y)
 
 open_canvas()
 boy = Char()
@@ -106,16 +161,19 @@ def handle_events():
                boy.state = boy.JUMP
             elif event.key == SDLK_LCTRL:
                 boy.attackcount += 1
-                boy.state = boy.Attack1
+                if boy.attackcount % 2 == 0:
+                    boy.state = boy.Attack1
+                elif boy.attackcount % 2 == 1:
+                    boy.state = boy.Attack2
 
         elif event.type == SDL_KEYUP:
             boy.xchange = 0
 
-
-grass = Grass()
 running = True
+back = Background()
 monsters = [Zombie() for i in range(4)]
 lizards = [Lizard() for i in range (4)]
+oct = [Octopus() for i in range(4)]
 
 while running:
     handle_events()
@@ -124,14 +182,18 @@ while running:
         zombie.update()
     for lizard in lizards:
         lizard.update()
+    for octopus in oct:
+        octopus.update()
 
     clear_canvas()
-    grass.draw()
+    back.draw()
     boy.draw()
     for zombie in monsters:
         zombie.draw()
     for lizard in lizards:
         lizard.draw()
+    for octopus in oct:
+        octopus.draw()
     update_canvas()
 
     delay(0.05)
